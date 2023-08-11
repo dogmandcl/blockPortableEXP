3dade4b5e7483e764422a0bbc46e156ea4fef66a
dogmandcl <dogman7174@gmail.com>
2023-08-11 15:32:59 -0400

    updates

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { Color4 } from '@dcl/sdk/math'
 import { addPlatforms } from './platforms'
 import { doorSystem } from './systems'
 import { showUI } from './denyUI'
+import { checkPortableExperience } from './peTracking'
 
 
 
@@ -12,6 +13,7 @@ engine.addSystem(doorSystem)
 
 export function main() {
   addPlatforms();
+  checkPortableExperience();
 
  
 
